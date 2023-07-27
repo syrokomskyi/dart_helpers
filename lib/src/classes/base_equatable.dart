5e4b0231b2b46bcbaeeb1893984f1006bcd8d96a
Andrii Syrokomskyi <signmotion@gmail.com>
2023-07-27 21:50:10 +0200

    Refactoring BaseEquatable.

--- lib/src/classes/base_equatable.dart
+++ lib/src/classes/base_equatable.dart
@@ -3,9 +3,12 @@ import 'dart:math';
 import 'package:equatable/equatable.dart';
 
 abstract class BaseEquatable extends Equatable {
-  const BaseEquatable([this.shrinkStringTo = 100]);
+  const BaseEquatable();
 
-  final int shrinkStringTo;
+  int get shrinkToString => 100;
+
+  @override
+  bool? get stringify => true;
 
   @override
   List<Object?> get props => [];
@@ -13,8 +16,8 @@ abstract class BaseEquatable extends Equatable {
   @override
   String toString() {
     final s = props.join('\t');
-    final data = s.substring(0, min(shrinkStringTo, s.length)) +
-        (s.length > shrinkStringTo ? '...' : '');
+    final data = s.substring(0, min(shrinkToString, s.length)) +
+        (s.length > shrinkToString ? '...' : '');
 
     return '$runtimeType $data';
   }
