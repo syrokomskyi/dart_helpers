d18f5d547124eae08e469ea98882ad43732616a1
Andrii Syrokomskyi <signmotion@gmail.com>
2023-10-29 09:31:18 +0100

    Added a string extension `removedDoubleChars`.

--- lib/src/extensions/string_ext.dart
+++ lib/src/extensions/string_ext.dart
@@ -1,6 +1,17 @@
 extension StringExt on String {
   String get removedDoubleSpaces => replaceAll(RegExp(r'\s+'), ' ');
 
+  String get removedDoubleChars {
+    var r = '';
+    for (var i = 0; i < length; ++i) {
+      if (i == 0 || this[i] != this[i - 1]) {
+        r += this[i];
+      }
+    }
+
+    return r;
+  }
+
   String truncate([int len = 200, String omission = '…']) =>
       len >= length ? this : replaceRange(len, length, omission);
 
