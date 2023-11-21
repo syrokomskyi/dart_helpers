d82012a27ede5f13a7296bd8bff94475985e4dfc
Andrii Syrokomskyi <signmotion@gmail.com>
2023-11-21 16:53:46 +0100

    Added jsonWithSignificantFields extension.

--- lib/src/extensions/json_ext.dart
+++ lib/src/extensions/json_ext.dart
@@ -6,6 +6,27 @@ import '../classes/jsons.dart';
 import '../classes/size.dart';
 import '../constants/string.dart';
 
+extension CleanProto3JsonExt on Map<String, dynamic> {
+  /// Removes all empty fields. The "empty fields" according to Proto3:
+  /// https://protobuf.dev/programming-guides/proto3/#default
+  /// !) Pay attention on the nested maps. See tests.
+  Map<String, dynamic> get jsonWithSignificantFields => map((k, v) =>
+      MapEntry(k, v is Map<String, dynamic> ? v.jsonWithSignificantFields : v))
+    ..removeWhere((k, v) =>
+        (v == null) ||
+        (v is bool && !v) ||
+        (v is int && v == 0) ||
+        (v is double && (v == 0.0 || v == -0.0)) ||
+        (v is String && v.isEmpty) ||
+        (v is Enum && v.index == 0) ||
+        (v is List && v.isEmpty) ||
+        (v is Map && v.isEmpty) ||
+        (v is Set && v.isEmpty) ||
+        false);
+
+  String get sJsonWithSignificantFields => jsonWithSignificantFields.sjson;
+}
+
 extension SizeJsonExt on Size {
   List<int> get json => [width.round(), height.round()];
 
--- test/json_ext_test.dart
+++ test/json_ext_test.dart
@@ -2,6 +2,82 @@ import 'package:dart_helpers/dart_helpers.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('jsonWithSignificantFields', () {
+    const json = <String, dynamic>{
+      // null
+      'null': null,
+      // bool
+      'false_bool': false,
+      'true_bool': true,
+      // int
+      'zero_int': 0,
+      'positive_int': 12,
+      'negative_int': -12,
+      // double
+      'positive_zero_double': 0.0,
+      'negative_zero_double': -0.0,
+      'positive_double': 1.2,
+      'negative_double': -1.2,
+      'positive_infinity_double': double.infinity,
+      'negative_infinity_double': double.negativeInfinity,
+      'nan_double': double.nan,
+      // string
+      'empty_string': '',
+      'spaced_string': '    ',
+      'filled_string': 'a bcd efghijkl',
+      // enum
+      'first_enum': TestEnum.unspecified,
+      'second_enum': TestEnum.a,
+      // list
+      'empty_list': <int>[],
+      'one_empty_element_list': <int>[0],
+      'two_empty_elements_list': <int>[0, 0],
+      'list': <int>[1, 2],
+      // map
+      'empty_map': <String, dynamic>{},
+      'one_empty_element_map': <String, dynamic>{'': 0},
+      'one_not_empty_value_element_map': <String, dynamic>{'': 12},
+      'one_not_empty_key_element_map': <String, dynamic>{'a': 0},
+      'two_empty_values_elements_map': <String, dynamic>{'a': 0, 'b': 0},
+      'map': <String, dynamic>{'a': 1, 'b': 2},
+      // set
+      'empty_set': <int>{},
+      'one_empty_element_set': <int>{0},
+      'set': <int>{1, 2},
+    };
+
+    expect(
+        json.jsonWithSignificantFields.keys,
+        {
+          // bool
+          'true_bool': true,
+          // int
+          'positive_int': 12,
+          'negative_int': -12,
+          // double
+          'positive_double': 1.2,
+          'negative_double': -1.2,
+          'positive_infinity_double': double.infinity,
+          'negative_infinity_double': double.negativeInfinity,
+          'nan_double': double.nan,
+          // string
+          'spaced_string': '    ',
+          'filled_string': 'a bcd efghijkl',
+          // enum
+          'second_enum': TestEnum.a,
+          // list
+          'one_empty_element_list': <int>[0],
+          'two_empty_elements_list': <int>[0, 0],
+          'list': <int>[1, 2],
+          // map
+          'one_not_empty_value_element_map': <String, dynamic>{'': 12},
+          'map': <String, dynamic>{'a': 1, 'b': 2},
+          // set
+          'one_empty_element_set': <int>{0},
+          'set': <int>{1, 2},
+        }.keys);
+  });
+
   test('sjsonWithoutWrappers array', () {
     expect([10, 11, 12].sjsonWithoutWrappers, '10,\r\n11,\r\n12');
   });
@@ -39,3 +115,5 @@ void main() {
     expect('null'.sjsonWithoutWrappers, 'null');
   });
 }
+
+enum TestEnum { unspecified, a, b }
